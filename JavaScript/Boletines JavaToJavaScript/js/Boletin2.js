8c21f0b3afda3ea61b2915f7a1047d70fc528549
AlbertoF17 <albertofergom@gmail.com>
2022-12-24 02:30:50 +0100

    SSOO

--- JavaScript/Boletines JavaToJavaScript/js/Boletin2.js
+++ JavaScript/Boletines JavaToJavaScript/js/Boletin2.js
@@ -28,7 +28,7 @@ realizarEj2a.onclick = function realizarEjercicio() {
 let borrarEj2a = document.querySelector("#borrar-ej2a");
 borrarEj2a.onclick = function borarEjercicio() {
     let ej = document.querySelector("#ej2a");
-    ej.innerHTML = ``
+    ej.innerHTML = ``;
 }
 
 //2b. Decrementar su valor en una unidad utilizando el operador de decremento --. Imprimirla por pantalla.
@@ -42,7 +42,7 @@ realizarEj2b.onclick = function realizarEjercicio() {
 let borrarEj2b = document.querySelector("#borrar-ej2b");
 borrarEj2b.onclick = function borarEjercicio() {
     let ej = document.querySelector("#ej2b");
-    ej.innerHTML = ``
+    ej.innerHTML = ``;
 }
 
 //3. Incrementar su valor en 4 unidades sin utilizar el operador de suma + y asignación =, e imprimirla por pantalla.
@@ -50,7 +50,7 @@ let realizarEj3 = document.querySelector("#realizar-ej3");
 realizarEj3.onclick = function realizarEjercicio() {
     alert("3. Incrementar su valor en 4 unidades sin utilizar el operador de suma + y asignación =, e imprimirla por pantalla.");
     let ej = document.querySelector("#ej3");
-    ej.innerHTML = `<p>Valor de "valor0": ${valor0}`
+    ej.innerHTML = `<p>Valor de "valor0": ${valor0}`;
     valor0++;
     valor0++;
     valor0++;
@@ -60,7 +60,7 @@ realizarEj3.onclick = function realizarEjercicio() {
 let borrarEj3 = document.querySelector("#borrar-ej3");
 borrarEj3.onclick = function borarEjercicio() {
     let ej = document.querySelector("#ej3");
-    ej.innerHTML = ``
+    ej.innerHTML = ``;
 }
 
 //4. Crear otra variable entera y asignarle el valor 48.76.
@@ -69,12 +69,12 @@ let realizarEj4 = document.querySelector("#realizar-ej4");
 realizarEj4.onclick = function realizarEjercicio() {
     alert("4. Crear otra variable entera y asignarle el valor 48.76.");
     let ej = document.querySelector("#ej4");
-    ej.innerHTML = `<p>let var1= ${var1}`
+    ej.innerHTML = `<p>let var1= ${var1}`;
 }
 let borrarEj4 = document.querySelector("#borrar-ej4");
 borrarEj4.onclick = function borarEjercicio() {
     let ej = document.querySelector("#ej4");
-    ej.innerHTML = ``
+    ej.innerHTML = ``;
 }
 
 //5. Modificarla dividiendo por 50 e imprimir el resultado por pantalla.
@@ -83,12 +83,12 @@ let realizarEj5 = document.querySelector("#realizar-ej5");
 realizarEj5.onclick = function realizarEjercicio() {
     alert("5. Modificarla dividiendo por 50 e imprimir el resultado por pantalla.");
     let ej = document.querySelector("#ej5");
-    ej.innerHTML = `<p>var1 = var1 / 50;</p>` + `<p>var1= ${var1}`
+    ej.innerHTML = `<p>var1 = var1 / 50;</p>` + `<p>var1= ${var1}`;
 }
 let borrarEj5 = document.querySelector("#borrar-ej5");
 borrarEj5.onclick = function borarEjercicio() {
     let ej = document.querySelector("#ej5");
-    ej.innerHTML = ``
+    ej.innerHTML = ``;
 }
 
 //6. Modificarla sumándole 1 y multiplicando por 48 e imprimir el resto de dividirla entre 46.
@@ -97,12 +97,12 @@ let realizarEj6 = document.querySelector("#realizar-ej6");
 realizarEj6.onclick = function realizarEjercicio() {
     alert("6. Modificarla sumándole 1 y multiplicando por 48 e imprimir el resto de dividirla entre 46.");
     let ej = document.querySelector("#ej6");
-    ej.innerHTML = `<p>var1 = ((var1 + 1) * 48);</p>` + `<p>var1%46= ${var1 % 46}`
+    ej.innerHTML = `<p>var1 = ((var1 + 1) * 48);</p>` + `<p>var1%46= ${var1 % 46}`;
 }
 let borrarEj6 = document.querySelector("#borrar-ej6");
 borrarEj6.onclick = function borarEjercicio() {
     let ej = document.querySelector("#ej6");
-    ej.innerHTML = ``
+    ej.innerHTML = ``;
 }
 
 
@@ -111,12 +111,12 @@ let realizarEj7 = document.querySelector("#realizar-ej7");
 realizarEj7.onclick = function realizarEjercicio() {
     alert("7. Sin modificar el valor de la variable, imprimir por pantalla el resultado de elevarla al cuadrado.");
     let ej = document.querySelector("#ej7");
-    ej.innerHTML = `<p>var1^2= ${var1 * var1}`
+    ej.innerHTML = `<p>var1^2= ${var1 * var1}`;
 }
 let borrarEj7 = document.querySelector("#borrar-ej7");
 borrarEj7.onclick = function borarEjercicio() {
     let ej = document.querySelector("#ej7");
-    ej.innerHTML = ``
+    ej.innerHTML = ``;
 }
 
 
@@ -129,12 +129,12 @@ realizarEj8.onclick = function realizarEjercicio() {
     x = prompt("Introduzca el primer numero: ");
     y = prompt("Introduzca el segundo numero: ");
     let ej = document.querySelector("#ej8");
-    ej.innerHTML = `<p>El primer número vale: ${x}</p><p>El segundo número vale: ${y}</p>`
+    ej.innerHTML = `<p>El primer número vale: ${x}</p><p>El segundo número vale: ${y}</p>`;
 }
 let borrarEj8 = document.querySelector("#borrar-ej8");
 borrarEj8.onclick = function borarEjercicio() {
     let ej = document.querySelector("#ej8");
-    ej.innerHTML = ``
+    ej.innerHTML = ``;
 }
 
 
@@ -144,17 +144,17 @@ realizarEj9.onclick = function realizarEjercicio() {
     alert("9. Imprimir por pantalla el resultado de comprobar si la primera es mayor que la segunda.");
     let ej = document.querySelector("#ej9");
     if (x > y) {
-        ej.innerHTML = `<p>El primer número es mayor que el segundo</p><p>${x} > ${y}</p><p>Resultado: ${x > y}</p>`
+        ej.innerHTML = `<p>El primer número es mayor que el segundo</p><p>${x} > ${y}</p><p>Resultado: ${x > y}</p>`;
     } else if (x < y) {
-        ej.innerHTML = `<p>El segundo número es mayor que el primero</p><p>${x} < ${y}</p><p>Resultado: ${x > y}`
+        ej.innerHTML = `<p>El segundo número es mayor que el primero</p><p>${x} < ${y}</p><p>Resultado: ${x > y}`;
     } else {
-        ej.innerHTML = `<p>Ambos números son iguales</p><p>${x} = ${y}</p><p>Resultado: ${x > y}`
+        ej.innerHTML = `<p>Ambos números son iguales</p><p>${x} = ${y}</p><p>Resultado: ${x > y}`;
     }
 }
 let borrarEj9 = document.querySelector("#borrar-ej9");
 borrarEj9.onclick = function borarEjercicio() {
     let ej = document.querySelector("#ej9");
-    ej.innerHTML = ``
+    ej.innerHTML = ``;
 }
 
 //10. Cambiar la primera con el valor de la segunda multiplicado por dos, y volver a imprimir por pantalla
@@ -164,11 +164,11 @@ realizarEj10.onclick = function realizarEjercicio() {
     alert("10. Cambiar la primera con el valor de la segunda multiplicado por dos, y volver a imprimir por pantalla el resultado de la comprobación anterior.");
     x = y * 2;
     let ej = document.querySelector("#ej10");
-    ej.innerHTML = `<p>El primer número vale: ${x}</p><p>El segundo número vale: ${y}</p>`
+    ej.innerHTML = `<p>El primer número vale: ${x}</p><p>El segundo número vale: ${y}</p>`;
     if (x > y) {
-        ej.innerHTML = `<p>El primer número es mayor que el segundo</p><p>${x} > ${y}</p><p>Resultado: ${x > y}`
+        ej.innerHTML = `<p>El primer número es mayor que el segundo</p><p>${x} > ${y}</p><p>Resultado: ${x > y}`;
     } else if (x < y) {
-        ej.innerHTML = `<p>El segundo número es mayor que el primero</p><p>${x} < ${y}</p><p>Resultado: ${x > y}`
+        ej.innerHTML = `<p>El segundo número es mayor que el primero</p><p>${x} < ${y}</p><p>Resultado: ${x > y}`;
     } else {
         ej.innerHTML = `<p>Ambos números son iguales</p><p>${x} = ${y}</p><p>Resultado: ${x > y}`
     }
@@ -176,7 +176,7 @@ realizarEj10.onclick = function realizarEjercicio() {
 let borrarEj10 = document.querySelector("#borrar-ej10");
 borrarEj10.onclick = function borarEjercicio() {
     let ej = document.querySelector("#ej10");
-    ej.innerHTML = ``
+    ej.innerHTML = ``;
 }
 
 //11. Imprimir por pantalla el resultado de comprobar si la primera es menor o igual que la segunda.
@@ -184,17 +184,17 @@ let realizarEj11 = document.querySelector("#realizar-ej11");
 realizarEj11.onclick = function realizarEjercicio() {
     alert("11. Imprimir por pantalla el resultado de comprobar si la primera es menor o igual que la segunda.");
     let ej = document.querySelector("#ej11");
-    ej.innerHTML = `<p>El primer número vale: ${x}</p><p>El segundo número vale: ${y}</p>`
+    ej.innerHTML = `<p>El primer número vale: ${x}</p><p>El segundo número vale: ${y}</p>`;
     if (x <= y) {
-        ej.innerHTML = `<p>El primer número es menor o igual que el segundo.</p><p>${x} <= ${y}</p><p>Resultado: ${x <= y}`
+        ej.innerHTML = `<p>El primer número es menor o igual que el segundo.</p><p>${x} <= ${y}</p><p>Resultado: ${x <= y}`;
     } else {
-        ej.innerHTML = `<p>El primer número es mayor que el segundo</p><p>${x} > ${y}</p><p>Resultado: ${x <= y}`
+        ej.innerHTML = `<p>El primer número es mayor que el segundo</p><p>${x} > ${y}</p><p>Resultado: ${x <= y}`;
     }
 }
 let borrarEj11 = document.querySelector("#borrar-ej11");
 borrarEj11.onclick = function borarEjercicio() {
     let ej = document.querySelector("#ej11");
-    ej.innerHTML = ``
+    ej.innerHTML = ``;
 }
 
 //12. Repetir el ejercicio anterior pero almacenando el resultado de la comprobación
@@ -204,16 +204,15 @@ realizarEj12.onclick = function realizarEjercicio() {
     alert("12. Repetir el ejercicio anterior pero almacenando el resultado de la comprobación en una variable, e imprimiendo después por pantalla dicha variable.");
     let menorIgual = x <= y;
     let ej = document.querySelector("#ej12");
-    ej.innerHTML = `<p>El primer número vale: ${x}</p><p>El segundo número vale: ${y}</p>`
+    ej.innerHTML = `<p>El primer número vale: ${x}</p><p>El segundo número vale: ${y}</p>`;
     if (menorIgual) {
-        ej.innerHTML = `<p>El primer número es menor o igual que el segundo.</p><p>variable "menorIgual" = ${menorIgual}</p>`
+        ej.innerHTML = `<p>El primer número es menor o igual que el segundo.</p><p>variable "menorIgual" = ${menorIgual}</p>`;
     } else {
-        ej.innerHTML = `<p>El primer número es mayor que el segundo</p><p>variable "menorIgual" = ${menorIgual}</p>`
+        ej.innerHTML = `<p>El primer número es mayor que el segundo</p><p>variable "menorIgual" = ${menorIgual}</p>`;
     }
 }
 let borrarEj12 = document.querySelector("#borrar-ej12");
 borrarEj12.onclick = function borarEjercicio() {
     let ej = document.querySelector("#ej12");
-    ej.innerHTML = ``
+    ej.innerHTML = ``;
 }
-//A PARTIR DEL 9 NO FUNCIONA POR EL USO DE VARIABLES "INDEFINIDAS"
