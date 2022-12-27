47570723427fb46e5fa825edeaf2671b6caedda6
AlbertoF17 <albertofergom@gmail.com>
2022-12-27 12:23:40 +0100

    SSOO

--- JavaScript/Boletines JavaToJavaScript/js/Boletin3.js
+++ JavaScript/Boletines JavaToJavaScript/js/Boletin3.js
@@ -4,12 +4,11 @@
 let realizarEj1 = document.querySelector("#realizar-ej1");
 let variable1;
 realizarEj1.onclick = function realizarEjercicio() {
-    //1. Crea una variable de tipo entero y asígnale el valor 10.
+    let ej = document.querySelector("#ej1");
     alert("1. Crea una variable de tipo entero y asígnale el valor 10.");
     let var1 = Number.parseInt(prompt("Inserte el valor:", 10));
     alert("2. Imprímela por pantalla.");
-    let ej = document.querySelector("#ej1");
-    ej.innerHTML = `<p>${var1}</p>`;
+    ej.innerHTML += `<p>${var1}</p>`;
     alert("3. Decrementa su valor una vez.");
     var1--;
     alert("4. Imprímela por pantalla.");
@@ -42,72 +41,109 @@ borrarEj1.onclick = function borrarEjercicio() {
     ej.innerHTML = ``
 }
 
-/*
 
-        // EJERCICIO 2
-        //Desarrollar un algoritmo para calcular el área de un rectángulo, cuya fórmula es base x altura.
-        //Para ello:
-        //1. Cree una variable para almacenar el valor de la base, y asigne un valor.
-        int base = 3;
-        //2. Cree una variable para almacenar el valor de la altura, y asigne un valor.
-        int altura = 4;
-        //3. Cree una variable para almacenar el área, y utilice la fórmula anterior para asignarle el valor
-        //adecuado.
-        int area = base*altura;
-        //4. Imprima por pantalla el área, utilizando una instrucción println.
-        System.out.println(area);
-        //5. Tenga en cuenta que debe elegir el tipo adecuado para cada variable.
-        //En principio usaré int porque no tengo necesidad de usar números complejos o con decimales.
-
-
-        //EJERCICIO 3
-        //Desarrollar un algoritmo para convertir de euros a dólares teniendo en cuenta que un euro son
-        //1.16 dólares. Para ello:
-        //1. Cree una variable para almacenar el factor de conversión (1.16).
-        float factor = 1.16f;
-        //2. Cree una variable para almacenar la cantidad de euros (no asigne ningún valor aún).
-        float euros;
-        //3. Utilice una instrucción de asignación para darle un valor a la variable anterior.
-        euros = 3.99f;
-        //4. Cree una variable para almacenar la cantidad de dólares, y asigne el resultado de multiplicar
-        //el factor de conversión por la cantidad de euros.
-        float dolares = euros*factor;
-        //5. Imprima por pantalla el resultado, utilizando una instrucción println.
-        System.out.println(euros + " euros, son " + dolares + " dólares");
+// EJERCICIO 2
+//Desarrollar un algoritmo para calcular el área de un rectángulo, cuya fórmula es base x altura.
+//Para ello:
+let realizarEj2 = document.querySelector("#realizar-ej2");
+realizarEj2.onclick = function realizarEjercicio() {
+    let ej = document.querySelector("#ej2");
+    alert("1. Cree una variable para almacenar el valor de la base, y asigne un valor.");
+    let base = Number.parseInt(prompt("Valor de la base:"));
+    alert("2. Cree una variable para almacenar el valor de la altura, y asigne un valor.");
+    let altura = Number.parseInt(prompt("Valor de la altura:"));
+    alert("3. Cree una variable para almacenar el área, y utilice la fórmula anterior para asignarle el valor adecuado.");
+    let area = (base*altura)/2; //Para realizar el ejercicio usando una variable (después no se usa)
+    alert("4. Imprima por pantalla el área, utilizando una instrucción println (innerHTML en JavaScript).");
+    ej.innerHTML = `<p>El área del rectángulo de base ${base} y de altura ${altura} es: ${(base*altura)/2} unidades cuadradas</p>`;
+}
+let borrarEj2 = document.querySelector("#borrar-ej2");
+borrarEj2.onclick = function borrarEjercicio() {
+    let ej = document.querySelector("#ej2");
+    ej.innerHTML = ``
+}
 
 
-        //EJERCICIO 4
-        //Modifique el ejercicio 2 para ahorrarnos la creación de la variable del área. Para ello:
-        //elimine dicha variable, y ponga en la instrucción println la fórmula del área directamente.
-        System.out.println(base*altura);
+//EJERCICIO 3
+//Desarrollar un algoritmo para convertir de euros a dólares teniendo en cuenta que un euro son
+//1.16 dólares. Para ello:
+let realizarEj3 = document.querySelector("#realizar-ej3");
+realizarEj3.onclick = function realizarEjercicio() {
+    let ej = document.querySelector("#ej3");
+    alert("1. Cree una variable para almacenar el factor de conversión (1.16).");
+    let factor = 1.16;//Para realizar el ejercicio usando una variable (después no se usa)
+    alert("2. Cree una variable para almacenar la cantidad de euros (no asigne ningún valor aún).");
+    let euros;
+    alert("3. Utilice una instrucción de asignación para darle un valor a la variable anterior.");
+    euros = prompt("Valor en euros:");
+    alert("4. Cree una variable para almacenar la cantidad de dólares, y asigne el resultado de multiplicar el factor de conversión por la cantidad de euros.");
+    let dolares = euros*factor;
+    alert("5. Imprima por pantalla el resultado, utilizando una instrucción println.");
+    ej.innerHTML = `<p>${euros} euros, son ${euros*1.16} dólares</p>`
+}
+let borrarEj3 = document.querySelector("#borrar-ej3");
+borrarEj3.onclick = function borrarEjercicio() {
+    let ej = document.querySelector("#ej3");
+    ej.innerHTML = ``
+}
 
 
-        //EJERCICIO 5
-        //Modifique el ejercicio 3 para ahorrarnos la creación de la variable del factor de conversión.
-        System.out.println(euros + " euros, son " + euros*1.16 + " dólares");
+//EJERCICIO 4
+//Modifique el ejercicio 2 para ahorrarnos la creación de la variable del área. Para ello:
+//elimine dicha variable, y ponga en la instrucción println la fórmula del área directamente.
+let realizarEj4 = document.querySelector("#realizar-ej4");
+realizarEj4.onclick = function realizarEjercicio() {
+    alert("Modifique el ejercicio 2 para ahorrarnos la creación de la variable del área.");
+    let ej = document.querySelector("#ej4");
+    ej.innerHTML = `<p>Hecho :)</p>`
+}
+let borrarEj4 = document.querySelector("#borrar-ej4");
+borrarEj4.onclick = function borrarEjercicio() {
+    let ej = document.querySelector("#ej4");
+    ej.innerHTML = ``
+}
 
 
+//EJERCICIO 5
+//Modifique el ejercicio 3 para ahorrarnos la creación de la variable del factor de conversión.
+let realizarEj5 = document.querySelector("#realizar-ej5");
+realizarEj5.onclick = function realizarEjercicio() {
+    alert("Modifique el ejercicio 3 para ahorrarnos la creación de la variable del factor de conversión.");
+    let ej = document.querySelector("#ej5");
+    ej.innerHTML = `<p>Hecho :)</p>`
+}
+let borrarEj5 = document.querySelector("#borrar-ej5");
+borrarEj5.onclick = function borrarEjercicio() {
+    let ej = document.querySelector("#ej5");
+    ej.innerHTML = ``
+}
 
-        //EJERCICIO 6
-        //1. Cree una variable de tipo entero y asigne el valor 3.
-        int x = 3;
-        //2. Decremente su valor tres veces con el operador de decremento (- -).
-        x--;
-        x--;
-        x--;
-        //3. Incremente su valor 6 unidades con el operador de suma simplificada (+=).
-        x += 6;
-        //4. Incremente su valor en una unidad utilizando el operador de incremento (++).
-        x++;
-        //5. Duplique su valor como desee.
-        x *= 2;
-        //6. Utilice el operador módulo (%) para comprobar el resto de dividir la variable entre 2,
-        //imprimiendo por pantalla el resultado.
-        System.out.print("x sera par si el resultado de realizar la operacion x%2 da 0, si no, el resultado sera 1 y por tanto x es impar, en este caso: ");
-        System.out.println(x%2);
-        //7. ¿Cómo se puede comprobar si un número es par o impar?
-        if (x%2 == 0) {
-            System.out.println("Por tanto, x es par: " + x);
-        }else {
-            System.out.println("Por tanto, x es impar: " + x);
-        }*/
+
+//EJERCICIO 6
+let realizarEj6 = document.querySelector("#realizar-ej6");
+realizarEj6.onclick = function realizarEjercicio() {
+    let ej = document.querySelector("#ej6");
+    alert("1. Cree una variable de tipo entero y asigne el valor 3.");
+    let x = 3;
+    alert("2. Decremente su valor tres veces con el operador de decremento (- -).");
+    x--;
+    x--;
+    x--;
+    alert("3. Incremente su valor 6 unidades con el operador de suma simplificada (+=).");
+    x += 6;
+    alert("5. Duplique su valor como desee.");
+    x *= 2;
+    alert("6. Utilice el operador módulo (%) para comprobar el resto de dividir la variable entre 2, imprimiendo por pantalla el resultado.");
+    ej.innerHTML = `<p>La variable creada "x" será par si el resultado de realizar la operacion x%2 da 0, sino, el resultado será 1 y por tanto "x" es impar, en este caso: ${x%2}</p>`
+    alert("7. ¿Cómo se puede comprobar si un número es par o impar?");
+    if (x%2 == 0) {
+        ej.innerHTML += `<p>Por tanto, x es par: ${x}</p>`;
+    }else {
+        ej.innerHTML += `<p>Por tanto, x es impar: ${x}</p>`;
+    }
+}
+let borrarEj6 = document.querySelector("#borrar-ej6");
+borrarEj6.onclick = function borrarEjercicio() {
+    let ej = document.querySelector("#ej6");
+    ej.innerHTML = ``
+}
