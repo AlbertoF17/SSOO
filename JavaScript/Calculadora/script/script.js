4f9824068c7fe46ecac1df059f0e3ce644a235f8
AlbertoF17 <albertofergom17@gmail.com>
2023-01-08 15:54:44 +0100

    SSOO

--- JavaScript/Calculadora/script/script.js
+++ JavaScript/Calculadora/script/script.js
@@ -1,8 +1,7 @@
 let display = document.querySelector("#display");
 
 let abreParentesis = document.querySelector("#(");
-
-abreParentesis.onclick = display.innerHTML+=`(`;
+abreParentesis = display.innerHTML+=`(`;
 
 let numeroNueve = document.querySelector("#9");
 numeroNueve.onclick = display.innerHTML+=`9`;
