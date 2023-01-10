92d333e2d7d9dafce496e7ca5aa26100f4ccd09f
AlbertoF17 <albertofergom17@gmail.com>
2023-01-10 17:49:42 +0100

    SSOO

--- JavaScript/Calculadora/script/script.js
+++ JavaScript/Calculadora/script/script.js
@@ -23,84 +23,84 @@ let operacion = "";
 
 parentesisAbierto.onclick = function muestraBoton(){
     operacion += "(";
-    display.innerHTML = `${operacion}`;
+    display.innerHTML = `<p>${operacion}</p>`;
 }
 parentesisCerrado.onclick = function muestraBoton(){
     operacion += ")";
-    display.innerHTML = `${operacion}`;
+    display.innerHTML = `<p>${operacion}</p>`;
 }
 letraC.onclick = function muestraBoton(){
     operacion = operacion.slice(0, -1);
-    display.innerHTML = `${operacion}`;
+    display.innerHTML = `<p>${operacion}</p>`;
 }
 logoCasa.onclick = function muestraBoton(){
     operacion = "";
-    display.innerHTML = `${operacion}`;
+    display.innerHTML = `<p>${operacion}</p>`;
 }
 num7.onclick = function muestraBoton(){
     operacion += "7";
-    display.innerHTML = `${operacion}`;
+    display.innerHTML = `<p>${operacion}</p>`;
 }
 num8.onclick = function muestraBoton(){
     operacion += "8";
-    display.innerHTML = `${operacion}`;
+    display.innerHTML = `<p>${operacion}</p>`;
 }
 num9.onclick = function muestraBoton(){
     operacion += "9";
-    display.innerHTML = `${operacion}`;
+    display.innerHTML = `<p>${operacion}</p>`;
 }
 opMult.onclick = function muestraBoton(){
     operacion += "*";
-    display.innerHTML = `${operacion}`;
+    display.innerHTML = `<p>${operacion}</p>`;
 }
 num4.onclick = function muestraBoton(){
     operacion += "4";;
-    display.innerHTML = `${operacion}`;
+    display.innerHTML = `<p>${operacion}</p>`;
 }
 num5.onclick = function muestraBoton(){
     operacion += "5";
-    display.innerHTML = `${operacion}`;
+    display.innerHTML = `<p>${operacion}</p>`;
 }
 num6.onclick = function muestraBoton(){
     operacion += "6";
-    display.innerHTML = `${operacion}`;
+    display.innerHTML = `<p>${operacion}</p>`;
 }
 opResta.onclick = function muestraBoton(){
     operacion += "-";
-    display.innerHTML = `${operacion}`;
+    display.innerHTML = `<p>${operacion}</p>`;
 }
 num1.onclick = function muestraBoton(){
     operacion += "1";
-    display.innerHTML = `${operacion}`;
+    display.innerHTML = `<p>${operacion}</p>`;
 }
 num2.onclick = function muestraBoton(){
     operacion += "2";
-    display.innerHTML = `${operacion}`;
+    display.innerHTML = `<p>${operacion}</p>`;
 }
 num3.onclick = function muestraBoton(){
     operacion += "3";
-    display.innerHTML = `${operacion}`;
+    display.innerHTML = `<p>${operacion}</p>`;
 }
 opSuma.onclick = function muestraBoton(){
     operacion += "+";
-    display.innerHTML = `${operacion}`;
+    display.innerHTML = `<p>${operacion}</p>`;
 }
 opResto.onclick = function muestraBoton(){
     operacion += "%";
-    display.innerHTML = `${operacion}`;
+    display.innerHTML = `<p>${operacion}</p>`;
 }
 num0.onclick = function muestraBoton(){
     operacion += "0";
-    display.innerHTML = `${operacion}`;
+    display.innerHTML = `<p>${operacion}</p>`;
 }
 puntoDecimal.onclick = function muestraBoton(){
     operacion += ".";
-    display.innerHTML = `${operacion}`;
+    display.innerHTML = `<p>${operacion}</p>`;
 }
 opIgual.onclick = function solucion(){
     let resultado = eval(operacion);
-    display.innerHTML = `${operacion}<p></p>`;
-    display.innerHTML += `${resultado}`;
+    display.innerHTML = `<p>${operacion}</p>`;
+    display.innerHTML += `<p>${resultado}</p>`;
     operacion = resultado;
     
 }
