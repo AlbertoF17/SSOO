46e233b7d84e5edaaca900348af18cae193586b7
AlbertoF17 <albertofergom17@gmail.com>
2023-01-10 17:42:00 +0100

    SSOO

--- JavaScript/Pruebas y Apuntes/js/main.js
+++ JavaScript/Pruebas y Apuntes/js/main.js
@@ -1,49 +1,46 @@
-//pop-up
-alert("Hola Mundo");
+// //pop-up
+// alert("Hola Mundo");
 
-//Variables
-let nombre = "Alberto";
-var apellido1 = "Fernández"; //NO USAR, JAVASCRIPT ANTIGUO
-const pi = 3.1416; //VALOR INCAMBIABLE
-let n = 1;
+// //Variables
+// let nombre = "Alberto";
+// var apellido1 = "Fernández"; //NO USAR, JAVASCRIPT ANTIGUO
+// const pi = 3.1416; //VALOR INCAMBIABLE
+// let n = 1;
 
-//Mostrar por consola
-console.log("Hola Mundo");
-console.clear();
-console.warn("Estás intentando cambiar el valor de una constante");
-console.error("Esto es un error");
+// //Mostrar por consola
+// console.log("Hola Mundo");
+// console.clear();
+// console.warn("Estás intentando cambiar el valor de una constante");
+// console.error("Esto es un error");
 
-//Pedir variables STRING
-let x = prompt("Introduce tu nombre: ", "Alberto"); //El segundo parámetro es valor por defecto
-let y = prompt("num1: ");
-let z = prompt("num2: ");
-alert("num 1 es: " + y + "; num 2 es: " + z + ".")
+// //Pedir variables STRING
+// let x = prompt("Introduce tu nombre: ", "Alberto"); //El segundo parámetro es valor por defecto
+// let y = prompt("num1: ");
+// let z = prompt("num2: ");
+// console.log("num 1 es: " + y + "; num 2 es: " + z + ".")
 
-//Pedir variables BOOLEANS
-let a = confirm("Es verdadero o falso? ")
+// //Pedir variables BOOLEANS
+// let a = confirm("Es verdadero o falso? ")
 
-//Pedir variables NUMÉRICAS
-let num1 = Number.parseInt(prompt("Escribe un numero: "));
-
-// Template
-let template = `El número es: ${num1}`;
-alert(template);
+// //Pedir variables NUMÉRICAS
+// let num1 = Number.parseInt(prompt("Escribe un numero: "));
 
 //Comparador
-alert("1" === 1); //Va a dar FALSE
-alert("1" !== 1); //Va a dar TRUE
-alert("1" == 1); //Va a dar TRUE
+console.log("1" === 1); //Va a dar FALSE
+console.log("1" !== 1); //Va a dar TRUE
+console.log("1" == 1); //Va a dar TRUE
 
 //Variables undefined y null
-let nulo = null;
-let indefinido = undefined;
+// let nulo = null;
+// let indefinido = undefined;
+// let undefined;
 
 //Cálculo tiempo
-console.time("tiempo");
-console.timeEnd("tiempo: ")
+// console.time("tiempo");
+// console.timeEnd("tiempo: ")
 
 //Operadores ternarios
-let bool2 = bool ? "bool es verdadero":"bool es falso";
+//let bool2 = bool ? "bool es verdadero":"bool es falso";
 
 //IF (igual que en JAVA)
 
@@ -55,9 +52,28 @@ let bool2 = bool ? "bool es verdadero":"bool es falso";
 
 //DO WHILE (igual que en JAVA)
 
-//Interactuar con HTML
+//STRINGS
+let str = new String("    Hola    ");
+console.log(str.length);
+console.log(str.indexOf("o"));
+console.log(str.includes("o")); //Método "CONTAINS" de Java
+console.log(str.trim);
+str = str.trim;
+console.log(str.replace("a","o")); //Sólo cambia primera ocurrencia
+console.log(str.repalceAll("a","o")); //Cambia todas las ocurrencias
+console.log(str.slice(1,2));
+console.log(str.split(" ")); //Separa las cadenas y devuelve un array con las cadenas separadas
+console.log(str.toUpperCase);
+console.log(str.toLowerCase);
+let x = 4;
+x.toString(); //Convierte número a string
 
-let div = document.querySelector("#NombreDeID");
-div.innerHTML = `<p> Hola </p>`;
-alert("Parando la ejecución");
-div.innerHTML = ``;
+// // Template String
+let template = `El número es: ${num1}`;
+console.log(template);
+
+//Interactuar con HTML
+// let div = document.querySelector("#NombreDeID");
+// div.innerHTML = `<p> Hola </p>`;
+// alert("Parando la ejecución");
+// div.innerHTML = ``;
