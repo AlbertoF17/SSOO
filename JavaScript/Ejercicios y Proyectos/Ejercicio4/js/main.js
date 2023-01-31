eb83004f3a24a398e87aef75f36b5fea0544b9a4
AlbertoF17 <albertofergom17@gmail.com>
2023-01-31 19:49:32 +0100

    SSOO

--- JavaScript/Ejercicios y Proyectos/Ejercicio4/js/main.js
+++ JavaScript/Ejercicios y Proyectos/Ejercicio4/js/main.js
@@ -1,3 +1,4 @@
+//Dado el array: const array = ["Joaquin", 1, ["Hola","Adios"], true]
 const array = ["Joaquin", 1,["Hola","Adios"], true];
 //1) Recorrer el array e insertar dichos valores en el localstorage. El key de cada valor corresponderá a variable + i siendo i el indice en el que se encuentra el array.
 for (elemento in array){
@@ -6,14 +7,22 @@ for (elemento in array){
 
 //2) Recorrer de 0 a la longitud del array y hacer la peticion de variable + i del localstorage.
 for (elemento in array){
-    localStorage.getItem("variable" + elemento);
+    console.log(localStorage.getItem("variable" + elemento));
 }
 
 //3) Crear un div por cada variable e insertar el valor de dicha variable en el div.
-for (elemento in array){
-    let divNuevo = document.createElement("DIV");
-    divNuevo.textContent = localStorage.getItem("variable" + elemento);
-}
-
+let div1 = document.createElement("DIV");
+div1.textContent = localStorage.getItem("variable0");
+let div2 = document.createElement("DIV");
+div2.textContent = localStorage.getItem("variable1");
+let div3 = document.createElement("DIV");
+div3.textContent = localStorage.getItem("variable2");
+let div4 = document.createElement("DIV");
+div4.textContent = localStorage.getItem("variable3");
 
 //4) Insertar estos divs en el body del html.
+const body = document.querySelector("body");
+body.appendChild(div1);
+body.appendChild(div2);
+body.appendChild(div3);
+body.appendChild(div4);
