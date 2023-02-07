f8eba8bbbf44f37597b50780d14c648fed00fa0d
AlbertoF17 <albertofergom17@gmail.com>
2023-02-07 17:30:24 +0100

    SSOO

--- JavaScript/Ejercicios y Proyectos/Ejercicio4/js/main.js
+++ JavaScript/Ejercicios y Proyectos/Ejercicio4/js/main.js
@@ -4,7 +4,11 @@
 const array = ["Joaquin", 1,["Hola","Adios"], true];
 //1) Recorrer el array e insertar dichos valores en el localstorage. El key de cada valor corresponderá a variable + i siendo i el indice en el que se encuentra el array.
 for (elemento in array){
-    localStorage.setItem("variable" + elemento, array[elemento]);
+    if(typeof(array[elemento] === "object")){
+        localStorage.setItem("variable" + elemento, JSON.stringify(array[elemento]));
+    }else{
+        localStorage.setItem("variable" + elemento, array[elemento]);
+    }
 }
 
 //2) Recorrer de 0 a la longitud del array y hacer la peticion de variable + i del localstorage.
