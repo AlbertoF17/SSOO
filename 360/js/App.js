693463933d53a834d4684c77fa53e2b0587486a7
AlbertoF17 <albertofergom17@gmail.com>
2023-02-08 20:18:49 +0100

    SSOO

---
+++ 360/js/App.js
@@ -0,0 +1,20 @@
+import './App.css';
+const CLIENT_ID = "0509929fbb7c4cb7af584c20631675b8";
+const REDIRECT_URI = "http://localhost:3000"
+const AUTH_ENDPOINT = "https://accounts.spotify.com/authorize"
+const RESPONSE_TYPE = "token"
+
+//https://accounts.spotify.com/authorize?client_id=0509929fbb7c4cb7af584c20631675b8&redirect_uri=http://localhost:3000&response_type=token
+
+function App() {
+
+    return (
+        <div className="App">
+            <header className="App-header">
+
+            </header>
+        </div>
+    );
+}
+
+export default App;
