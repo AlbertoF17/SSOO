b1ff0a0e1802bd08a6e3351bd150c88505e48f7c
AlbertoF17 <albertofergom@gmail.com>
2023-04-11 12:20:45 +0200

    SSOO

--- JavaScript/Ejercicios y Proyectos/Ejercicio7/js/App.js
+++ JavaScript/Ejercicios y Proyectos/Ejercicio7/js/App.js
@@ -1,9 +1,9 @@
 import {Alumno} from "./Alumno.js";
 
-fetch("../json/alumnos.json")
+fetch("json/alumnos.json")
   .then(response => response.json())
   .then(data => {
-    data.alumnos.forEach(alumno => {
+    data.forEach(alumno => {
       const nuevoAlumno = new Alumno(alumno.nombre, alumno.apellido, alumno.dni, alumno.notaProg,
       alumno.notaSist, alumno.notaMarc, alumno.notaBbdd, alumno.notaEntornos, alumno.notaFol);
 
@@ -19,9 +19,37 @@ fetch("../json/alumnos.json")
       const dni = document.createElement('p');
       dni.textContent = `DNI: ${nuevoAlumno.dni}`;
 
+      const tituloNotas = document.createElement('h3');
+      tituloNotas.textContent = "Notas:";
+
+      const notaProg = document.createElement('p');
+      notaProg.textContent = `Programación: ${nuevoAlumno.notaProg}`;
+
+      const notaSist = document.createElement('p');
+      notaSist.textContent = `Sistemas: ${nuevoAlumno.notaSist}`;
+
+      const notaMarc = document.createElement('p');
+      notaMarc.textContent = `Marketing: ${nuevoAlumno.notaMarc}`;
+
+      const notaBbdd = document.createElement('p');
+      notaBbdd.textContent = `Bases de Datos: ${nuevoAlumno.notaBbdd}`;
+
+      const notaEntornos = document.createElement('p');
+      notaEntornos.textContent = `Entornos de Desarrollo: ${nuevoAlumno.notaEntornos}`;
+
+      const notaFol = document.createElement('p');
+      notaFol.textContent = `FOL: ${nuevoAlumno.notaFol}`;
+
       tarjeta.appendChild(nombre);
       tarjeta.appendChild(apellido);
       tarjeta.appendChild(dni);
+      tarjeta.appendChild(tituloNotas);
+      tarjeta.appendChild(notaProg);
+      tarjeta.appendChild(notaSist);
+      tarjeta.appendChild(notaMarc);
+      tarjeta.appendChild(notaBbdd);
+      tarjeta.appendChild(notaEntornos);
+      tarjeta.appendChild(notaFol);
 
       document.body.appendChild(tarjeta);
     });
